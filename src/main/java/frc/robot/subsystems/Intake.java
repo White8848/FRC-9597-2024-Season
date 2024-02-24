213c8c9cfac28c3ebc56b81add6ca315ef9a3f8b
water_ask <2356758998@qq.com>
2024-02-24 18:29:10 +0800

    adjust pid slot and add sim message

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,21 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.DoubleArrayPublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StringPublisher;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.units.Per;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import java.time.Period;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -10,9 +24,19 @@ import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 public class Intake extends SubsystemBase {
     private static final String canBusName = "canivore";
     private final TalonFX m_intakeTalonFX = new TalonFX(8, canBusName);
-
     private final VelocityTorqueCurrentFOC m_torqueVelocity = new VelocityTorqueCurrentFOC(0, 0, 0, 0, false, false,
             false);
+
+    /* What to publish over networktables for telemetry */
+    private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
+
+    /* intake data for checking */
+    private final NetworkTable driveStats = inst.getTable("Intake");
+    private final DoublePublisher velocityTarget = driveStats.getDoubleTopic("Velocity Target").publish();
+    private final DoublePublisher velocityMeasurement = driveStats.getDoubleTopic("Velocity Measurement").publish();
+    private final DoublePublisher torqueTarget = driveStats.getDoubleTopic("Torque Target").publish();
+    private final DoublePublisher torqueMeasurement = driveStats.getDoubleTopic("Torque Measurement").publish();
+    private final DoublePublisher torqueFeedForward = driveStats.getDoubleTopic("Torque FeedForward").publish();
 
     public Intake() {
         initializeTalonFX(m_intakeTalonFX.getConfigurator());
@@ -43,17 +67,22 @@ public class Intake extends SubsystemBase {
                 () -> setVelocity(0.0));
     }
 
-    private void setVelocity(double desiredRotationsPerSecond) {
+    public void setVelocity(double desiredRotationsPerSecond) {
 
         if (Math.abs(desiredRotationsPerSecond) <= 1) { // Joystick deadzone
             desiredRotationsPerSecond = 0;
         }
-        double friction_torque = (desiredRotationsPerSecond > 0) ? 1 : -1; // To account for friction, we add this to
-                                                                           // the
-        // arbitrary feed
-        // forward
-        m_intakeTalonFX.setControl(
-                m_torqueVelocity.withVelocity(desiredRotationsPerSecond).withFeedForward(friction_torque));
+
+        m_intakeTalonFX.setControl(m_torqueVelocity.withVelocity(desiredRotationsPerSecond));
+    }
+
+    @Override
+    public void periodic() {
+        velocityTarget.set(m_intakeTalonFX.getClosedLoopReference().getValueAsDouble());
+        velocityMeasurement.set(m_intakeTalonFX.getVelocity().getValueAsDouble());
+        torqueTarget.set(m_intakeTalonFX.getClosedLoopOutput().getValueAsDouble());
+        torqueMeasurement.set(m_intakeTalonFX.getTorqueCurrent().getValueAsDouble());
+        torqueFeedForward.set(m_intakeTalonFX.getClosedLoopFeedForward().getValueAsDouble());
     }
 
     private void initializeTalonFX(TalonFXConfigurator cfg) {
@@ -63,7 +92,7 @@ public class Intake extends SubsystemBase {
          * Torque-based velocity does not require a feed forward, as torque will
          * accelerate the rotor up to the desired velocity by itself
          */
-        toApply.Slot0.kP = 5; // An error of 1 rotation per second results in 5 amps output
+        toApply.Slot0.kP = 10; // An error of 1 rotation per second results in 5 amps output
         toApply.Slot0.kI = 0.1; // An error of 1 rotation per second increases output by 0.1 amps every second
         toApply.Slot0.kD = 0.001; // A change of 1000 rotation per second squared results in 1 amp output
 
